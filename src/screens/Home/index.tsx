a83f080b99eaf8999066c6555294af99394ae18c
baastos <bastosleft@gmail.com>
2021-06-17 00:09:14 -0300

    🚩 - adds header on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { Container } from './styles'
+import { StatusBar } from 'react-native'
+import { Container, Header, HeaderContent, TotalCarsText } from './styles'
+import Logo from '../../assets/logo.svg'
 
 export function Home() {
   return (
     <Container>
-      <Text>HOME</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Header>
+        <HeaderContent>
+          <Logo />
+          <TotalCarsText>Total de 12 carros</TotalCarsText>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
