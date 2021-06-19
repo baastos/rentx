46caa2663ca4adee053a48e84c23d2303b9db4b8
baastos <bastosleft@gmail.com>
2021-06-19 19:48:22 -0300

    🚩 - create card car component

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Type,
+  Price,
+  CarImage,
+} from './styles'
+import Icon from '../../assets/gasoline.svg'
+
+interface CarProps {
+
+  brand: string,
+  name: string,
+  period: string,
+  price: string,
+  image_url: string
+
+}
+
+interface Car {
+  data: CarProps
+}
+
+export function Car({ data }: Car) {
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+        <About>
+          <Rent>
+            <Period>{data.period}</Period>
+            <Price>{data.price}</Price>
+          </Rent>
+          <Type>
+            <Icon />
+
+          </Type>
+        </About>
+
+      </Details>
+      <CarImage source={{ uri: data.image_url }} />
+    </Container>
+  )
+}
---
+++ src/components/Car/styles.ts
@@ -0,0 +1,60 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: ${RFValue(126)}px;
+  padding: 17px 24px;
+  background: ${({theme})=> theme.colors.background_secondary };
+
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+
+`;
+
+export const Details = styled.View``;
+
+
+export const Brand = styled.Text`
+  font-size: ${RFValue(10)}px;
+  font-family: ${({theme})=> theme.fonts.secondary_medium};
+  text-transform: uppercase;
+  color:${({theme})=> theme.colors.text_detail};
+`;
+
+export const Name = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme})=> theme.fonts.secondary_medium};
+  color:${({theme})=> theme.colors.title};
+`;
+
+export const About = styled.View`
+  flex-direction: row;
+  margin-top: 16px;
+  align-items: flex-end;
+`;
+
+export const Rent = styled.View`
+`;
+
+export const Period = styled.Text`
+  font-size: ${RFValue(10)}px;
+  font-family: ${({theme})=> theme.fonts.secondary_medium};
+  text-transform: uppercase;
+  color:${({theme})=> theme.colors.text_detail};
+`;
+
+export const Price = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme})=> theme.fonts.secondary_medium};
+  color:${({theme})=> theme.colors.main};
+`;
+
+export const CarImage = styled.Image`
+  height: 92px;
+  width: 160px;
+`;
+
+export const Type = styled.View`
+margin-left: 25px;
+`;
