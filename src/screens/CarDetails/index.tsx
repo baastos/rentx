137796199f8873d2f6c8d0b59f89bc041d0de7fe
baastos <bastosleft@gmail.com>
2021-06-20 15:37:18 -0300

    🚩 - create car details screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import { useTheme } from 'styled-components'
+import { Acessory } from '../../components/Acessory'
 import { BackButton } from '../../components/BackButton'
-import { Container, Header } from './styles'
+import { ImageSlider } from '../../components/ImageSlider'
+
+import accelerationSvg from '../../assets/acceleration.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import forceSvg from '../../assets/force.svg'
+import peopleSvg from '../../assets/people.svg'
+import speedSvg from '../../assets/speed.svg'
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Acessories,
+  About
+} from './styles'
 
 
 export function CarDetails() {
@@ -11,8 +35,40 @@ export function CarDetails() {
     <Container>
       <Header>
         <BackButton color={theme.colors.text} />
-
       </Header>
+
+      <CarImages>
+
+        <ImageSlider imagesUrl={["https://catalogo.webmotors.com.br/imagens/prod/348415/AUDI_RS5_2.9_V6_TFSI_GASOLINA_SPORTBACK_QUATTRO_STRONIC_34841521101233346.png?s=fill&w=440&h=330&q=80&t=true"]} />
+      </CarImages>
+
+      <Content>
+        <Details>
+
+          <Description>
+            <Brand>Audi</Brand>
+            <Name>RS 5 Coupé</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 120</Price>
+          </Rent>
+        </Details>
+        <Acessories>
+          <Acessory name="380km/h" icon={speedSvg} />
+          <Acessory name="3.2s" icon={accelerationSvg} />
+          <Acessory name="800 HP" icon={forceSvg} />
+          <Acessory name="Gasolina" icon={gasolineSvg} />
+          <Acessory name="Auto" icon={exchangeSvg} />
+          <Acessory name="2 pessoas" icon={peopleSvg} />
+        </Acessories >
+        <About>
+          Este é automóvel desportivo.
+          Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   )
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,3 +1,5 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -10,6 +12,78 @@ export const Header = styled.View`
   align-items: center;
   justify-content: space-between;
 
-  padding: 61px 24px
+  position: absolute;
+  margin-top: ${getStatusBarHeight() + 18}px;
+  margin-left: 24px;
 
+`;
+
+export const CarImages = styled.View`
+  margin-top: ${getStatusBarHeight() + 32}px; ;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {padding: 24, alignItems: 'center'},
+  showsVerticalScrollIndicator: false,
+
+})``;
+
+export const Details = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+  
+  margin-top: 36px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({theme})=> theme.fonts.secondary_regular};
+  color: ${({theme})=> theme.colors.text_detail};
+  text-transform: uppercase;
+  font-size: ${RFValue(10)}px;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({theme})=> theme.fonts.secondary_regular};
+  color: ${({theme})=> theme.colors.title};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({theme})=> theme.fonts.secondary_regular};
+  color: ${({theme})=> theme.colors.text_detail};
+  text-transform: uppercase;
+  font-size: ${RFValue(10)}px;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({theme})=> theme.fonts.secondary_regular};
+  color: ${({theme})=> theme.colors.main};
+  font-size: ${RFValue(25)}px;
+`;
+export const Acessories = styled.View`
+  width: 100%;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  margin-top: 16px;
+
+`;
+
+
+export const About = styled.Text`
+  font-family: ${({theme})=> theme.fonts.primary_regular};
+  color: ${({theme})=> theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  text-align: justify;
+  line-height: ${RFValue(25)}px;
+
+  margin-top: 23px;
 `;
