5774b3903bb45ed6ccd9d26bb649fda11d882087
baastos <bastosleft@gmail.com>
2021-06-30 22:28:00 -0300

    💄 - set width and height to back button component

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,6 +1,14 @@
 import styled from 'styled-components/native';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 
-export const Button = styled(BorderlessButton)``;
+export const Button = styled(BorderlessButton)`
+
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+  align-items: center;
+  justify-content: center;
+  margin-left: -16px;
+`;
 
