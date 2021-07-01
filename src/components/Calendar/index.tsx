b110fd35ec3131625999a0f0946cc9aa7bd083cc
baastos <bastosleft@gmail.com>
2021-06-30 22:27:16 -0300

    🏷️ - add calendar prop types

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
-import {Calendar as CustomCalendar, LocaleConfig} from 'react-native-calendars'
+import {Calendar as CustomCalendar, LocaleConfig, DateCallbackHandler} from 'react-native-calendars'
 import {Feather} from '@expo/vector-icons';
 import {useTheme} from 'styled-components'
+import { ptBR } from './locales';
 
-LocaleConfig.locales['pt-br'] = {
-  monthNames:['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
-  today: 'Hoje'
+
+interface DayProps{
+  dateString: string;
+  day: number;
+  month: number;
+  year: number;
+  timestamp: number;
 }
+interface MarkedDatesProps {
+  [date: string]: {
+    color: string;
+    textColor: string;
+    disabled?: boolean;
+    disableTouchEvent?: boolean
+  }
+}
+interface CalendarProps{
+  markedDates: MarkedDatesProps;
+  onDayPress: DateCallbackHandler
+
+}
+
+
+LocaleConfig.locales['pt-br'] = ptBR
 LocaleConfig.defaultLocale = 'pt-br'
-export function Calendar(){
+
+ function Calendar({markedDates, onDayPress}: CalendarProps){
+
   const theme = useTheme()
+
   return (
     <CustomCalendar
     renderArrow={(direction)=>
@@ -44,6 +65,15 @@ export function Calendar(){
 
     firstDay={1}
     minDate={new Date()}
+    markingType="period"
+    markedDates={markedDates}
+    onDayPress={onDayPress}
     />
   )
+}
+
+export {
+  MarkedDatesProps,
+  DayProps,
+  Calendar
 }
