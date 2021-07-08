d63467bb35f039a3c1ad2f1ab9fb8f9d2a196100
baastos <bastosleft@gmail.com>
2021-07-08 00:07:24 -0300

    💫 - add car slider animation

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useRef, useState  } from 'react'
+import { FlatList, ViewToken } from 'react-native'
 import { Container, ImageIndexes, ImageIndex, CarImageWrapper, CarImage } from './styles'
 
 interface Props {
   imagesUrl: string[];
 }
-
+interface ImageChangeProps {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+}
 export function ImageSlider({ imagesUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const imageChange = useRef((info: ImageChangeProps)=>{
+     const currentIndex = info.viewableItems[0].index!
+
+     setImageIndex(currentIndex)
+  })
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {imagesUrl.map((_,index)=> <ImageIndex key={String(index)} active={index === imageIndex} />)}
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage source={{ uri: imagesUrl[0] }} resizeMode="contain" />
-      </CarImageWrapper>
+        <FlatList
+        data={imagesUrl}
+        keyExtractor={item => item}
+        renderItem={({item})=> (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={imageChange.current}
+
+        />
+        
+
     </Container>
   )
 }
